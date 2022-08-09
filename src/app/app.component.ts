78c909a793d561d32489d48a614082acebf64b5d
unknown <liubomyr.lypetskyi@gmail.com>
2022-08-09 16:15:08 +0300

    added the third stage of the task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,46 @@
-import { Component } from '@angular/core';
-import { IProduct } from './models/product';
-import {products as data } from './data/products';
+import { Component, OnInit } from '@angular/core';
 
+import { IProduct } from './models/product';
+import { ProductsService } from './services/products.service';
+import { products } from './data/products';
+import { Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'New Angular app Crash_project_2022';
 
-  products: IProduct[] = data;
+  // I підхід
+  // products: IProduct[] = [];
+
+  // IІ підхід
+  products$!: Observable<IProduct[]>
+
+  loading = false;
+
+  constructor(private productsService: ProductsService) {
+  }
+
+
+  // I підхід
+  // ngOnInit(): void {
+  //   this.loading = true;
+  //   this.productsService.getAll()
+  //     .subscribe((products) => {
+  //       this.products = products;
+  //       this.loading = false;
+  //     });
+  // }
+
+
+  // IІ підхід
+  ngOnInit(): void {
+    this.loading = true;
+    this.products$ = this.productsService.getAll()
+      .pipe(tap(() => this.loading = false));
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductComponent } from './conponents/product/product/product.component';
+import { ProductComponent } from './conponents/product/product.component';
+import { GlobalErrorComponent } from './conponents/global-error/global-error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent
+    ProductComponent,
+    GlobalErrorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
